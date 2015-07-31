6d7f35583ea8bfb61d4d68f6e796236689c2ee66
Shiva <shiv@shiv.me>
2015-07-30 21:29:42 -0700

    Parser and display symbols in a header file

--- cmds/accessors.js
+++ cmds/accessors.js
@@ -3,15 +3,58 @@
  */
 'use strict';
 
+var libclang = require('libclang');
+
 module.exports = function(program) {
 
 	program
-		.command('accessors <type>')
-		.version('0.0.1')
-		.description('Generate accessors for the specified type')
-		.action(function(type) {
-            console.log("input: ", program.input);
-            console.log("type: ", type);
-		});
-	
+  	.command('accessors <type>')
+    .version('0.0.1')
+    .description('Generate accessors for the specified type')
+    .action(function(type) {
+
+
+
+			console.log("input: ", program.input);
+			console.log("type: ", type);
+
+			var
+				Cursor = libclang.Cursor,
+				Index = libclang.Index,
+				TranslationUnit = libclang.TranslationUnit;
+
+			var dclang = require('libclang/lib/dynamic_clang');
+			var consts = dclang.CONSTANTS;
+
+			var index = new Index(true, true);
+			var tu = new TranslationUnit.fromSource(index, program.input, []);
+
+
+			tu.cursor.visitChildren(function (parent) {
+				if (this.kind != Cursor.MacroDefinition &&
+					this.kind != Cursor.MacroInstantiation &&
+					this.kind != Cursor.LastProcessing) {
+					console.log(consts.CXCursorKind[this.kind], this.spelling);
+				}
+
+				switch (this.kind) {
+					case Cursor.LastPreprocessing:
+						return Cursor.Break;
+						break;
+					case Cursor.Namespace:
+					case Cursor.ClassDecl:
+					case Cursor.FunctionDecl:
+					case Cursor.FunctionTemplate:
+					case Cursor.EnumDecl:
+						return Cursor.Recurse;
+						break;
+					default:
+						return Cursor.Continue;
+						break;
+				}
+			});
+
+			index.dispose();
+			//tu.dispose();
+    });
 };
--- etc/sample1.h
+++ etc/sample1.h
@@ -4,3 +4,6 @@ typedef struct foo_s {
     int         a;
     char       *c;
 } foo_t;
+
+void foo();
+void bar();
